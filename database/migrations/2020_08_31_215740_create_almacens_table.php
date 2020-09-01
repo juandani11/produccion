bfe5f617c960e6aab621818f6f5bf5fa7760a51f
juandani11 <jdpa9429@gmail.com>
2020-09-01 01:05:11 -0400

    practice

---
+++ database/migrations/2020_08_31_215740_create_almacens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlmacensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('almacens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('codigo');
+            $table->string('descripcion');
+            $table->boolean('estado');
+            $table->string('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('almacens');
+    }
+}
